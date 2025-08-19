8323b04dd298232b9a99c700348a002655f37847
stgroovy <tosinadeyemo88@gmail.com>
2025-08-19 17:54:43 +0100

    fixed error

--- Program.cs
+++ Program.cs
@@ -1,25 +1,29 @@
 ﻿using Fit_X;
 
-static void Main()
+class Program 
 {
-    Console.WriteLine(" Fit X");
-    Console.Write("Input workout type (e.g, Jogging, Cycling): ");
-    string type = Console.ReadLine();
-
-    Console.Write("Enter workout duration: ");
-    double duration = double.Parse(Console.ReadLine());
-      if (duration <= 0)
+    static void Main()
     {
-        Console.Write("Please enter a valid number for duration:");
+        Console.WriteLine(" Fit X");
+        Console.Write("Input workout type (e.g, Jogging, Cycling): ");
+        string type = Console.ReadLine();
+
+        Console.Write("Enter workout duration: ");
+        double duration = double.Parse(Console.ReadLine());
+        if (duration <= 0)
+        {
+            Console.Write("Please enter a valid number for duration:");
+        }
+
+        Fitness workout = new Fitness(type, duration);
+
+        Console.WriteLine("Workout logged successfully!");
+        Console.WriteLine(workout.summary());
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
     }
 
-    Fitness workout = new Fitness(type, duration);
-
-    Console.WriteLine("Workout logged successfully!");
-    Console.WriteLine(workout.summary());
-
-    Console.WriteLine("\nPress any key to exit...");
-    Console.ReadKey();
 }
 
 
